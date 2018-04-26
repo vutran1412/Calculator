f01c2c009c6b25dc0ad19c71ae5519c5c4658c41
Vu Tran <vutran1412@icloud.com>
2018-04-26 01:42:28 -0500

    Calculator Class finished

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -8,7 +8,77 @@ namespace Calculator
 {
     public class Calculator
     {
-        private decimal CurrentValue;
+        public decimal currentValue;
+        public decimal enterValue;
+
+        public void Add(decimal enterValue)
+        {
+            currentValue += enterValue;
+        }
+
+        public void Subtract(decimal enterValue)
+        {
+            currentValue -= enterValue;
+        }
+
+        public void Multiply(decimal enterValue)
+        {
+            currentValue *= enterValue;
+        }
+
+        public void Divide(decimal enterValue)
+        {
+            currentValue /= enterValue;
+        }
+
+        public void Reciprocal(decimal enterValue)
+        {
+            currentValue = 1 / enterValue;
+        }
+
+        public void SquareRoot(decimal enterValue)
+        {
+            currentValue = (decimal)Math.Sqrt(Convert.ToDouble(enterValue));
+        }
+
+        public decimal Equals()
+        {
+            return currentValue;
+        }
+
+        public void Clear()
+        {
+            currentValue = 0;
+            enterValue = 0;
+        }
+
+        public decimal CurentValue
+        {
+            set
+            {
+                currentValue = value;
+            }
+            get
+            {
+                return currentValue;
+            }
+        }
+
+        public decimal EnterValue
+        {
+            set
+            {
+                enterValue = value;
+            }
+            get
+            {
+                return enterValue;
+            }
+        }
+        
+
+
+
 
     }
 }
